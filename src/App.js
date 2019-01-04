40df8cc197e25d14f9d75d22419aac3f2a8d40bd
Placid Rodrigues <placidrod@gmail.com>
2019-01-04 14:11:17 +1300

    Add code from my linux machine

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <p>Hello Hot reload in docker</p>
+          <p>Code in linux</p>
           <a
             className="App-link"
             href="https://reactjs.org"
