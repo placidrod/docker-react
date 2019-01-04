85f6b80b01a4f0866f3082d72cf8d0672db1723a
Placid Rodrigues <placidrod@gmail.com>
2019-01-04 14:19:42 +1300

    Check push without pass

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <p>Hello Hot reload in docker</p>
-          <p>Code in linux</p>
+          <p>Code in linux. Check without pass.</p>
           <a
             className="App-link"
             href="https://reactjs.org"
